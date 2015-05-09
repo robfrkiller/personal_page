e12bfdb50f62f2b1a1138369726a78ffc22c03b0
robfrkiller <robfrkiller@gmail.com>
2015-05-09 15:13:38 +0800

    first commit.

---
+++ js/main.js
@@ -0,0 +1,16 @@
+(function () {
+	var bg_img = [
+			'http://i.imgur.com/JIm9Xql.jpg',
+			'http://i.imgur.com/rTWHcdr.jpg',
+			'http://i.imgur.com/0Qi6hfk.jpg',
+			'http://i.imgur.com/PeBHAyT.jpg',
+			'http://i.imgur.com/PEhve9K.jpg',
+			'http://i.imgur.com/r22gnK2.jpg',
+			'http://i.imgur.com/0CNw6bg.jpg',
+			'http://i.imgur.com/rloEVJW.jpg',
+			'http://i.imgur.com/T1UboIt.jpg',
+			'http://i.imgur.com/zwbXLuI.jpg'
+		],
+		rand_index = Math.round(Math.random() * (bg_img.length - 1) + 1);
+	document.body.style.backgroundImage = 'url(' + bg_img[rand_index] + '.jpg)';
+}());
