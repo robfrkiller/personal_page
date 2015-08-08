34271ed05e5e93d13d3ee17d6b1772dff97396ca
robfrkiller <robfrkiller@gmail.com>
2015-08-08 23:51:23 +0800

    埋ga

--- js/main.js
+++ js/main.js
@@ -19,3 +19,10 @@
 		rand_index = Math.round(Math.random() * (bg_img.length - 1));
 	document.body.style.backgroundImage = 'url(' + bg_img[rand_index] + '.jpg)';
 }());
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-17060361-7', 'auto');
+ga('send', 'pageview');
